1c3a4e691ff530f22f77589a3a0104c04749cec5
raul <Ztr4w04@gmail.com>
2023-11-07 01:19:29 -0600

    Created the following stubs: - The Point2D class.
    Implemented the following stubs:
    - Rotation::convertIndexesToCordinates(int, int, int).
    - Rotation::xAxis(double, double, double).
    - Rotation::yAxis(double, double, double).
    - Rotation::zAxis(double, double, double).

---
+++ src/main/java/nephewbob/voxelspipe/Point2D.java
@@ -0,0 +1,13 @@
+package nephewbob.voxelspipe;
+
+/**
+ *
+ * @author Raul Verduzco Cruz <ztr4w at nephewbob>
+ */
+class Point2D {
+
+    Point2D(double yPrime, double zPrime) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
--- src/main/java/nephewbob/voxelspipe/Rotation.java
+++ src/main/java/nephewbob/voxelspipe/Rotation.java
@@ -20,19 +20,53 @@ class Rotation {
         this.psi = psi;
     }    
 
-    Point3D convertIndexesToCordinates(int i, int j, int k) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Point3D convertIndexesToCordinates(int horizontal, int vertical, int depth){
+        double x = horizontal + 1;
+        double y = vertical + 1;
+        double z = depth + 1;
+        return new Point3D(x, y, z);
     }
 
-    void yAxis(double xPrime, double yPrime, double zPrime) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Point2D xAxis(double x, double y, double z) {
+        double phiRadians = Math.toRadians(- phi);
+        double cosPhi = Math.cos(phiRadians);
+        double sinPhi = Math.sin(phiRadians);
+        double zSinPhi = z * sinPhi;
+        double zCosPhi = z * cosPhi;
+        double ySinPhi = y * sinPhi;
+        double yCosPhi = y * cosPhi;
+        xPrime = x;
+        yPrime = yCosPhi - zSinPhi;
+        zPrime = ySinPhi + zCosPhi;
+        return new Point2D(yPrime, zPrime);
+    }
+    
+    public Point2D yAxis(double x, double y, double z) {
+        double thetaRadiants = Math.toRadians(theta);
+        double cosTheta = Math.cos(thetaRadiants);
+        double sinTheta = Math.sin(thetaRadiants);
+        double xCosTheta = x * cosTheta;
+        double xSinTheta = x * sinTheta;
+        double zSinTheta = z * sinTheta;
+        double zCosTheta = z * cosTheta;
+        xPrime = xCosTheta + zSinTheta;
+        yPrime = y;
+        zPrime = -xSinTheta + zCosTheta;
+        return new Point2D(xPrime, zPrime);
     }
 
-    void zAxis(double xPrime, double yPrime, double zPrime) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Point2D zAxis(double x, double y, double z) {
+        double psiRadians = Math.toRadians(psi);
+        double cosPsi = Math.cos(psiRadians);
+        double sinPsi = Math.sin(psiRadians);
+        double xSinPsi = x * sinPsi;
+        double xCosPsi = x * cosPsi;
+        double ySinPsi = y * sinPsi;
+        double yCosPsi = y * cosPsi;
+        xPrime = xCosPsi - ySinPsi;
+        yPrime = xSinPsi + yCosPsi;
+        zPrime = z;
+        return new Point2D(xPrime, yPrime);
     }
 
-    void xAxis(double x, double y, double z) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
 }
